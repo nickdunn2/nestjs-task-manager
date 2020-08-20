d489f79426c8903e61d9121fe194ad0fbd81af3c
Nick Dunn <nick.dunn@flosports.tv>
2020-08-20 09:18:10 -0500

    feat: add validation to username/password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator'
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    { message: 'password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or symbol'}
+    )
   password: string
 }
