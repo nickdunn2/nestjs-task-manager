883f1ee606556e47a0a74d79913c30981066ca50
Nick Dunn <nick.dunn@flosports.tv>
2020-08-26 09:12:55 -0500

    feat: introduce GetUser decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common'
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 import { AuthService } from './auth.service'
-import { AuthGuard } from '@nestjs/passport'
 
 @Controller('auth')
 export class AuthController {
@@ -17,11 +16,5 @@ export class AuthController {
   @Post('/login')
   login(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
     return this.authService.login(authCredentialsDto)
-  }
-
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log(req)
   }
 }
