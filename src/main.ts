7d968b2a719565cfbee61e2eedc44926ec130ed3
Nick Dunn <nick.dunn@flosports.tv>
2020-10-17 13:20:57 -0500

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const port = process.env.PORT || serverConfig.port
 
+  // Could be only IF process.env.NODE_ENV === 'development' (npm run start:dev)
+  app.enableCors()
+
   await app.listen(port)
   logger.log(`Application listening on port ${port}`)
 }
